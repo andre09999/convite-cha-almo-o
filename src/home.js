86464badbbed5edeab5f8f3ea42a23e1d1f4bee8
andre09999 <andre09999@live.com>
2023-03-15 03:11:49 -0300

    update

--- src/home.js
+++ src/home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './Header';
 import './App.css'
 import wpp from './images/whatsapp.png'
 import utensilhos from './images/utensilhos-removebg-preview.png'
